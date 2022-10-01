01d768cb5c4371df80893058e61961478eec841a
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-10-01 04:43:58 +0000

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  get 'sceneries/index'
- get 'genres/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admins
+  devise_for :users
+  
+  devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
+
+  namespace :admin do
+  get 'genres/new'
 end
+
+
+
+  scope module: :public do
+  get 'sceneries/index'
+  end 
+  
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
