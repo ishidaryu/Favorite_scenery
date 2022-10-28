de21ca5353c7042fcebea96bef7b65c85cc54089
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-10-28 07:17:20 +0000

    お問い合わせ_セキュリティ

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact, user)
     @contact = contact
-    mail to: 'shitianlongzhijie456@gmail.com', subject: "お問い合わせについて【自動送信】"
+    mail to: ENV['GOOGLE_API_KEY'], subject: "お問い合わせについて【自動送信】"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
     port:                  587,
-  #  domain:               'smtp.gmail.com',
-    user_name:            'shitianlongzhijie456@gmail.com',
-    password:             'lfnpeqvazcnihpep',
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    :user_name =>         ENV['MAIL_ADDRESS'],
+    :password  =>         ENV['MAIL_PASSWORD'],
+    :authentication =>    :plain,
     enable_starttls_auto:  true
    }
 
