8ee3c5a04734ccfe5365e2d1d95a425da1dbf651
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-10-28 17:34:16 +0000

    微調整

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,14 +1,14 @@
 class Admin::UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(20)
   end
 
   def show
     @user = User.find_by(id: params[:id])
     @sceneries = @user.sceneries
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     head :no_content
