acb6b55022f821b0550401e28a09b8db8ed5f064
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-11-01 14:54:41 +0000

    maps

--- db/migrate/20220930131958_create_sceneries.rb
+++ db/migrate/20220930131958_create_sceneries.rb
@@ -4,6 +4,9 @@ class CreateSceneries < ActiveRecord::Migration[6.1]
       t.integer :genre_id, null: true
       t.string :name, null: false
       t.string :introduction, null: false
+      t.string :address
+      t.float :latitude
+      t.float :longitude
       t.integer :user_id
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_143110) do
+ActiveRecord::Schema.define(version: 2022_10_21_133114) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -72,14 +72,6 @@ ActiveRecord::Schema.define(version: 2022_10_31_143110) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "maps", force: :cascade do |t|
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
@@ -91,6 +83,9 @@ ActiveRecord::Schema.define(version: 2022_10_31_143110) do
     t.integer "genre_id"
     t.string "name", null: false
     t.string "introduction", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
